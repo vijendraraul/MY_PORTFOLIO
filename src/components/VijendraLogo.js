8d64ae44582ee2ed3128b7cea6524be376ceca5f
Vijendra Rajput <raulvijendra@gmail.com>
2024-05-28 14:06:27 +0530

    first commit

---
+++ src/components/VijendraLogo.js
@@ -0,0 +1,15 @@
+import React from "react";
+import logo from '../assets/img/VijendraBG.png';
+import { Container } from "react-bootstrap";
+
+export const VijendraLogo = () => {
+    return (
+        <Container>
+       
+          <img src={logo} alt="Logo" />
+        
+        </Container >
+    )
+}
+
+export default VijendraLogo;
